e92e25fe01a035bc0b47599a799a2eabae4a472f
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-08-10 14:14:52 +0300

    clean starlette example

--- examples/starlette.py
+++ examples/starlette.py
@@ -1,7 +1,5 @@
 import typing
 from starlette.applications import Starlette
-from starlette.middleware import Middleware
-from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
 from starlette.routing import Route
 
@@ -18,24 +16,15 @@ class WithHintError(Exception):
 def index_view(request: Request) -> typing.NoReturn:
     request.state.token = 'mytoken'
     request.app.state.app_token = 'app mytoken'
-    request.session['user_id'] = 1
-    request.session['user_password'] = 'password'
     raise TypeError('Oops, something really went wrong...')
 
 
 def hint_view(request: Request) -> typing.NoReturn:
-    request.state.token = 'mytoken'
-    request.app.state.app_token = 'app mytoken'
-    request.session['user_id'] = 1
-    request.session['user_password'] = 'password'
     raise WithHintError('Oops, something really went wrong...')
 
 
 app = Starlette(
     routes=[Route('/', index_view), Route('/hint', hint_view)],
-    middleware=[
-        Middleware(SessionMiddleware, secret_key=True),
-    ],
     exception_handlers={
         Exception: create_exception_handler(debug=True),
     },
